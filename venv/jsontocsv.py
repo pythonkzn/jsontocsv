43b7065ee98e05ce0d95702cd0bc247f166fd773
raravilov <raravilov@gmail.com>
2019-03-04 14:01:51 +0300

    поиск по двум параметрам

--- venv/jsontocsv.py
+++ venv/jsontocsv.py
@@ -7,37 +7,17 @@ import csv
 def getJson (path):
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f) #data в формате dict
-
         cards = data['data'] #cards в формате str, избавились от первого общего ключа data
         data_clear = json.loads(cards) #формат dict без первого общего ключа data
         cards_dict = data_clear['cards']
-      #  data_dict = json.loads(cards)
         return cards_dict
-
-
-    #записали в файл рабочий список
-    #with open('cards_dict.json', 'w', encoding='utf-8') as write_file:
-    #    json.dump(cards_dict, write_file, ensure_ascii=False)
-
-
-
-
-
-#данная функция формирует по входным параметрам запроса ищет данные в json файле и формирует словарь
- #   def SearchToDict (path_input, search_for, search_for_attribute, search_output1, search_output2, main_cards_dict):
- #       output_main['Первый атрибут'] = []
- #       output_main['Второй атрибут'] = []
- #       for dtp_dicts in main_cards_dict:
- #           if dtp_dicts[search_for] == search_for_attribute:
- #               output_main['Первый атрибут'].append(dtp_dicts[search_output1])
- #               output_main['Второй атрибут'].append(dtp_dicts[search_output2])
- #       print (output_main)
- #       return output_main
 
 def JsonToTable(path_input, main_cards_dict, label):
     output_list1 = [] #ключи
     output_list2 = [] #значения
+    index = 0 #индекс который присваивает каждой карточке ДТП уникальный номер
     for dtp_dicts in main_cards_dict:
+        index += 1
         for keys in dtp_dicts.keys():
             if keys == 'infoDtp':
                 for subkeys in dtp_dicts[keys].keys():
@@ -46,20 +26,18 @@ def JsonToTable(path_input, main_cards_dict, label):
                             if ts_info_key == 'ts_uch':
                                 if dtp_dicts[keys][subkeys][0]['ts_uch']: #проверка на заполненность атрибута ts_uch. в некоторых карточках он пуст
                                     for ts_uch_key in list(dtp_dicts[keys][subkeys][0][ts_info_key][0].keys()):
-                                        output_list2.append(str(dtp_dicts[keys][subkeys][0][ts_info_key][0][ts_uch_key]) + ' ' + str(dtp_dicts[label]))
+                                        output_list2.append(str(dtp_dicts[keys][subkeys][0][ts_info_key][0][ts_uch_key]) + ' ' + str(dtp_dicts[label])+ ' ' + str (index))
                                         output_list1.append(str(keys) + ' ' + str(subkeys) + ' ' + str(ts_info_key) + ' ' + str(ts_uch_key))
                             else:
-                                output_list2.append(dtp_dicts[keys][subkeys][0][ts_info_key]+' ' + str(dtp_dicts[label]))
+                                output_list2.append(dtp_dicts[keys][subkeys][0][ts_info_key]+' ' + str(dtp_dicts[label])+ ' ' + str (index))
                                 output_list1.append(str(keys) + ' '+ str(subkeys) + ' ' + str(ts_info_key))
                     else:
-                        output_list2.append(str(dtp_dicts[keys][subkeys])+' '+ str(dtp_dicts[label]))
+                        output_list2.append(str(dtp_dicts[keys][subkeys])+' '+ str(dtp_dicts[label])+ ' ' + str (index))
                         output_list1.append(str(keys) + ' '+ str(subkeys))
             else:
-                output_list2.append(str(dtp_dicts[keys]) +' '+ str(dtp_dicts[label]))
+                output_list2.append(str(dtp_dicts[keys]) +' '+ str(dtp_dicts[label]) + ' ' + str (index))
                 output_list1.append(keys)
     return output_list2
-
-
 
 
 def DictToCSV(output_main):
@@ -70,14 +48,24 @@ def DictToCSV(output_main):
         for line in dict.values():
             writer.writerow(line)
 
-def SearchInList (SearchIt, List):
+def SearchInList (SearchIt, SearchIt2, List):
     output_list = []
+    output_list2 = []
+    res_list = []
     for element in List:
         spl_element = element.split()[0]
         if SearchIt == spl_element:
-            output_list.append(element.split()[1])
-    print (output_list)
+            output_list.append(element.split())
+    for element2 in List:
+        spl_element2 = element2.split()[0]
+        if SearchIt2 == spl_element2:
+            output_list2.append(element2.split())
 
+    for el in output_list:
+        for el2 in output_list2:
+            if el[-1] == el2[-1]:
+                res_list.append(el[1])
+    print (res_list)
 
 
 def main():
@@ -85,9 +73,11 @@ def main():
     path_input = input('Укажите путь к загружаемому файлу:  ')
     in_label = input ('Укажите что Вы ищете ')
     search_for = input ('Укажите по какому параметру:  ')
+    search_for2 = input('Укажите второй параметр:  ')
+
     main_cards_dict = getJson(path_input)
     output_main = JsonToTable(path_input, main_cards_dict, in_label)
-    SearchInList(search_for, output_main)
+    SearchInList(search_for, search_for2, output_main)
 
 
 main()
